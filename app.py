ef464185bd7be4af5c47676cb6247992c3981983
abiwonggrage-a11y <abi.wonggrage@gmail.com>
2026-05-23 15:51:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ with col1:
 
 with col2:
     st.image("WhatsApp Image 2026-05-23 at 14.36.23.jpeg", caption="selalu berdelapan terus ya!!!", use_container_width=True)
-    st.image("https://drive.google.com/file/d/1J3cUQKq9wg-OLzLM-ADJBuc9pUTK1tpk/view?usp=sharing", caption="makasih! karena kalian, masa masa sma aku bener bener indah💕", use_container_width=True)
+    st.image("WhatsApp Image 2026-05-23 at 14.36.24 (1).jpeg", caption="makasih! karena kalian, masa masa sma aku bener bener indah💕", use_container_width=True)
 
 st.markdown("---")
 
